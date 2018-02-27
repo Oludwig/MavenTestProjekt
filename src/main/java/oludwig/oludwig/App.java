6e6b4dabfa61dcb03eb516a2e4c59b63d24d651c
Oliver Ludwig <oludwig@hsr.ch>
2018-02-27 12:39:02 +0100

    added basic maven project

---
+++ src/main/java/oludwig/oludwig/App.java
@@ -0,0 +1,21 @@
+package oludwig.oludwig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public static int one(){
+		return 2;
+	}
+	
+	public static int two(){
+		return 2;
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/oludwig/oludwig/AppTest.java
@@ -0,0 +1,43 @@
+package oludwig.oludwig;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAppOne()
+    {
+        assertEquals(1,App.one());
+    }
+    
+    public void testAppTwo()
+    {
+        assertEquals(2,App.two());
+    }
+}
